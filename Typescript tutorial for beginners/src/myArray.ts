cee12911f86cfda799060a7cb69f0b0cbf9836a8
Aneesh  Srivastava <aneesh.s@safe.security>
2023-03-30 12:00:58 +0530

    Adding basic array code

---
+++ Typescript tutorial for beginners/src/myArray.ts
@@ -0,0 +1,18 @@
+const superHeros: string[] =[]
+superHeros.push("Aneesh-Man")
+
+const heroPower: Array<number> = []
+heroPower.push(2)
+
+type courseUser = {
+    name: string;
+    isActive: boolean;
+}
+const allUsers: User[] = []
+
+// Two dimensional array:
+const pixelsOfPicture: number[][] =[
+    [233, 140, 255], 
+    [255, 255, 255]
+]
+

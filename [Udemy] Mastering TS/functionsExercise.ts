f633a1d811a8b7dc9cb7a6db13f465b853408004
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-06-05 22:11:39 +0530

    feature: Objects Execersie

--- [Udemy] Mastering TS/functionsExercise.ts
+++ [Udemy] Mastering TS/functionsExercise.ts
@@ -30,12 +30,16 @@ console.log(twoFer());
 // - year is a multiple of 400
 // hint - use modulo
 
-function isLeapYear(year: number): boolean {
-  if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+// function isLeapYear(year: number): boolean {
+//   if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+const isLeapYear = (year: number): boolean =>{
+    return (year % 4 === 0 && year % 100 !=0) || year % 400 ===0
 }
 
 console.log(isLeapYear(2012));

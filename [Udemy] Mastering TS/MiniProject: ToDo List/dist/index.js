4541a3a2c4474719782877b4e0b1bb995c2e298e
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-06-24 17:08:30 +0530

    feature: miniproject started

---
+++ [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Hello Aneesh!');
+console.log("I am working very hard!");
+console.log("Today's Date is: 24 Jun 2023");
+const greetMe = (name) => {
+    console.log(`Howdy ${name}!`);
+};
+greetMe('Aneesh');

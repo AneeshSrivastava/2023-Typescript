bb6fede03c06ba1bb4c84eaea215c9b7727ce51f
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-06-25 23:09:39 +0530

    feature: Building a generic Todo

--- [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
+++ [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
@@ -2,8 +2,17 @@
 const btn = document.getElementById("btn"); // Type of btn is HTMLElement or null
 const inputBox = document.getElementById("todo");
 const form = document.querySelector("form");
-form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
+const list = document.getElementById("todolist");
+function handleFormSubmit(e) {
     e.preventDefault(); // Default behavior of JS is to clear the form post submission, so we prevent that
     console.log("Form SUBMITTED!");
+    const newTodoText = inputBox.value;
+    const newLI = document.createElement("li");
+    const newCheckBox = document.createElement("input");
+    newCheckBox.type = "Checkbox";
+    newLI.append(newCheckBox);
+    newLI.append(newTodoText);
+    list.append(newLI);
     inputBox.value = "";
-});
+}
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", handleFormSubmit);
--- [Udemy] Mastering TS/MiniProject: ToDo List/src/index.ts
+++ [Udemy] Mastering TS/MiniProject: ToDo List/src/index.ts
@@ -1,10 +1,21 @@
 const btn = document.getElementById("btn")! // Type of btn is HTMLElement or null
 const inputBox = document.getElementById("todo") as HTMLInputElement;
 const form = document.querySelector("form");
+const list = document.getElementById("todolist")!
 
-form?.addEventListener("submit", (e) => { // e is assumed as "SubmitEvent" by Typescript
-   e.preventDefault(); // Default behavior of JS is to clear the form post submission, so we prevent that
-   console.log("Form SUBMITTED!");
-   inputBox.value="";
-});
+function handleFormSubmit(e: SubmitEvent){
+    e.preventDefault(); // Default behavior of JS is to clear the form post submission, so we prevent that
+    console.log("Form SUBMITTED!");
+    const newTodoText = inputBox.value;
+    const newLI = document.createElement("li");
+    const newCheckBox = document.createElement("input");
+    newCheckBox.type="Checkbox";
+    newLI.append(newCheckBox);
+    newLI.append(newTodoText)
+    list.append(newLI)
+    inputBox.value="";
+
+}
+
+form?.addEventListener("submit", handleFormSubmit);
 

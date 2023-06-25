fde305461248dfad456325e26acf83ceb4f03ca8
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-06-25 14:28:56 +0530

    feature: understanding non-null asertion

--- [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
+++ [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
@@ -2,3 +2,7 @@
 const btn = document.getElementById("btn");
 console.log(btn);
 console.log("Hello!".replaceAll('!', '')); // replaceAll() method is only available post EX2021
+const btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", () => {
+    alert("btn2 is clicked!");
+});

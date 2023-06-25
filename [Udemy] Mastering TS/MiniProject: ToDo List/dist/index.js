5d41542db7317fede0ecb482f8ced90b3448271c
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-06-25 23:19:16 +0530

    feature: Using todo interface and Array

--- [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
+++ [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+const todos = [];
 const btn = document.getElementById("btn"); // Type of btn is HTMLElement or null
 const inputBox = document.getElementById("todo");
 const form = document.querySelector("form");
@@ -6,13 +7,20 @@ const list = document.getElementById("todolist");
 function handleFormSubmit(e) {
     e.preventDefault(); // Default behavior of JS is to clear the form post submission, so we prevent that
     console.log("Form SUBMITTED!");
-    const newTodoText = inputBox.value;
+    const newToDo = {
+        text: inputBox.value,
+        completed: false
+    };
+    todos.push(newToDo);
+    createToDo(newToDo);
+    inputBox.value = "";
+}
+function createToDo(todo) {
     const newLI = document.createElement("li");
     const newCheckBox = document.createElement("input");
     newCheckBox.type = "Checkbox";
     newLI.append(newCheckBox);
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     list.append(newLI);
-    inputBox.value = "";
 }
 form === null || form === void 0 ? void 0 : form.addEventListener("submit", handleFormSubmit);
--- [Udemy] Mastering TS/MiniProject: ToDo List/src/index.ts
+++ [Udemy] Mastering TS/MiniProject: ToDo List/src/index.ts
@@ -1,3 +1,9 @@
+interface ToDo {
+    text: string,
+    completed: boolean
+}
+const todos: ToDo[] =[]
+
 const btn = document.getElementById("btn")! // Type of btn is HTMLElement or null
 const inputBox = document.getElementById("todo") as HTMLInputElement;
 const form = document.querySelector("form");
@@ -6,15 +12,23 @@ const list = document.getElementById("todolist")!
 function handleFormSubmit(e: SubmitEvent){
     e.preventDefault(); // Default behavior of JS is to clear the form post submission, so we prevent that
     console.log("Form SUBMITTED!");
-    const newTodoText = inputBox.value;
+    const newToDo: ToDo ={
+        text: inputBox.value,
+        completed: false
+    }
+    todos.push(newToDo)
+    createToDo(newToDo)
+    inputBox.value="";
+
+}
+
+function createToDo(todo: ToDo){
     const newLI = document.createElement("li");
     const newCheckBox = document.createElement("input");
     newCheckBox.type="Checkbox";
     newLI.append(newCheckBox);
-    newLI.append(newTodoText)
+    newLI.append(todo.text)
     list.append(newLI)
-    inputBox.value="";
-
 }
 
 form?.addEventListener("submit", handleFormSubmit);

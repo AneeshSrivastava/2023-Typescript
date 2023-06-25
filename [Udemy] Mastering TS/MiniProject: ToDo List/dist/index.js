9e5f9b3ca567fd71fdc207c18d962f1d2bb93d77
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-06-25 22:43:03 +0530

    feature: Type assertion with DOM

--- [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
+++ [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
@@ -1,8 +1,14 @@
 "use strict";
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn"); // Type of btn is HTMLElement or null
 console.log(btn);
 console.log("Hello!".replaceAll('!', '')); // replaceAll() method is only available post EX2021
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
+    alert("btn is clicked!");
+});
+// Type assertion with DOM elements 
+const toDoInput = document.getElementById("todo"); // Type Assertion
 const btn2 = document.getElementById("btn2");
 btn2.addEventListener("click", () => {
-    alert("btn2 is clicked!");
+    alert(toDoInput.value); // No need to check for null
+    toDoInput.value = ""; // Reset the value of input box after clicking!
 });
--- [Udemy] Mastering TS/MiniProject: ToDo List/src/index.ts
+++ [Udemy] Mastering TS/MiniProject: ToDo List/src/index.ts
@@ -1,10 +1,16 @@
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn") // Type of btn is HTMLElement or null
 console.log(btn);
 
 console.log("Hello!".replaceAll('!', '')); // replaceAll() method is only available post EX2021
+btn?.addEventListener("click", () => {
+    alert("btn is clicked!")
+})
 
-const btn2 = document.getElementById("btn2")!
+// Type assertion with DOM elements 
+const toDoInput = document.getElementById("todo") as HTMLInputElement; // Type Assertion
+const btn2 = document.getElementById("btn2")!;
 
 btn2.addEventListener("click", () => {
-    alert("btn2 is clicked!")
-})
+    alert(toDoInput.value) // No need to check for null
+    toDoInput.value="" // Reset the value of input box after clicking!
+});

d29bc7e87b9dbb842a7071ee0ab1aaec2d0f4c8d
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-06-25 14:17:26 +0530

    feature: understanding lib compiler option

--- [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
+++ [Udemy] Mastering TS/MiniProject: ToDo List/dist/index.js
@@ -1,3 +1,4 @@
 "use strict";
 const btn = document.getElementById("btn");
 console.log(btn);
+console.log("Hello!".replaceAll('!', '')); // replaceAll() method is only available post EX2021
--- [Udemy] Mastering TS/MiniProject: ToDo List/src/index.ts
+++ [Udemy] Mastering TS/MiniProject: ToDo List/src/index.ts
@@ -1,2 +1,4 @@
 const btn = document.getElementById("btn")
 console.log(btn);
+
+console.log("Hello!".replaceAll('!', '')); // replaceAll() method is only available post EX2021

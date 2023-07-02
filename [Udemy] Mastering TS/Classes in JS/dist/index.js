6020eff2155bc18f58415aef6c4fa1d50f1a9027
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-07-02 22:35:09 +0530

    feature: JS class constructor

--- [Udemy] Mastering TS/Classes in JS/dist/index.js
+++ [Udemy] Mastering TS/Classes in JS/dist/index.js
@@ -2,13 +2,19 @@
 console.log("Its working!");
 
 class Player {
+    constructor(first, last){
+        this.first = first;
+        this.last = last;
+    }
     taunt(){
         console.log("BOOOO!");
     }
 }
 
-const player1= new Player()
+const player1= new Player("Blue", "Cat")
+console.log(player1.first);
 player1.taunt()
-const player2= new Player()
+const player2= new Player("Black", "Dog")
+console.log(player2.first);
 player2.taunt()
 

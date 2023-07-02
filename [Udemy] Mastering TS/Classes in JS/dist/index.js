5dd021d49ad92f4f05c8f24c91e47fba6727acb3
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2023-07-02 21:49:41 +0530

    feature: New dir setup and basic Player class

---
+++ [Udemy] Mastering TS/Classes in JS/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("Its working!");
+
+class Player {
+    taunt(){
+        console.log("BOOOO!");
+    }
+}
+
+const player1= new Player()
+player1.taunt()
+const player2= new Player()
+player2.taunt()
+
